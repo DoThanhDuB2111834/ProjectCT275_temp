4cc2d223fbd561e19696ad0322783ab20876a9ed
DoThanhDuB2111834 <dub2111834@student.ctu.edu.vn>
2024-03-30 13:22:04 +0700

    init Lib v2

---
+++ app/model/admin.php
@@ -0,0 +1,10 @@
+<?php
+namespace App\Models;
+use Illuminate\Database\Eloquent\Model;
+
+class admin extends Model
+{
+    protected $table = 'admin';
+
+    protected $fillable = ['name', 'phone', 'notes', 'user_id'];
+}
