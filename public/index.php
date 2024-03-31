4e26bb9a7721af402a5086d7b661c56ab339068d
DoThanhDuB2111834 <dub2111834@student.ctu.edu.vn>
2024-03-31 16:42:06 +0700

    Thêm chức năng đăng nhập

--- public/index.php
+++ public/index.php
@@ -1,13 +1,28 @@
 <?php
 
-// require_once __DIR__ . '/../vendor/autoload.php';
+ini_set('display_errors', '1');
+ini_set('display_startup_errors', '1');
+error_reporting(E_ALL);
+
 require_once __DIR__ . '/../bootstrap.php';
 
-use App\Models\TaiKhoan;
-use App\Models\SanPham;
-use App\Models\HoaDon;
+define('APPNAME', 'My Phonebook');
 
-$sp = HoaDon::find(3)->SanPham;
-$tk = HoaDon::all()->first()->TaiKhoan;
+session_start();
 
-echo "$sp";
+$router = new \Bramus\Router\Router();
+
+//login và logout
+$router->get('/login', '\App\Controllers\Auth\LoginController@create');
+$router->post('/login', '\App\Controllers\Auth\LoginController@store');
+$router->post('/logout', '\App\Controllers\Auth\LoginController@destroy');
+
+// Admin thêm tài khoản mới cho nhân viên
+$router->get('/AddAccount', '\App\Controllers\Auth\AddNewAccountController@create');
+
+$router->get('/home', '\App\Controllers\HomeController@index');
+$router->get('/', '\App\Controllers\HomeController@index');
+
+$router->set404('\App\Controllers\Controller@sendNotFound');
+
+$router->run();
